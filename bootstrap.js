a86e0703cd61b9bdedba44aee6fb392e8bee030f
Eitan Isaacson <eitan@monotonous.org>
2011-09-14 12:42:35 -0700

    Use Services module

--- bootstrap.js
+++ bootstrap.js
@@ -3,6 +3,8 @@ const Ci = Components.interfaces;
 const Cu = Components.utils;
 const Cr = Components.results;
 
+Cu.import("resource://gre/modules/Services.jsm");
+
 var loader = Cc["@mozilla.org/moz/jssubscript-loader;1"]
     .getService(Ci.mozIJSSubScriptLoader);
 
@@ -26,25 +28,21 @@ function loadIntoWindow (aWindow) {
 function startup (data, reason) {
     dump ("started! " + data.id.substring(0, data.id.indexOf('@')) + "\n");
 
-    let ios = Cc['@mozilla.org/network/io-service;1'].getService(Ci.nsIIOService)
-    let resource = ios.getProtocolHandler("resource").
+    let resource = Services.io.getProtocolHandler("resource").
         QueryInterface(Ci.nsIResProtocolHandler);
 
-    let alias = ios.newFileURI(data.installPath);
+    let alias = Services.io.newFileURI(data.installPath);
     resource.setSubstitution(data.id.substring(0, data.id.indexOf('@')), alias);
 
-    let wm = Cc["@mozilla.org/appshell/window-mediator;1"]
-        .getService(Ci.nsIWindowMediator);
-
     // Load into any existing windows
-    let enumerator = wm.getEnumerator("navigator:browser");
+    let enumerator = Services.wm.getEnumerator("navigator:browser");
     while (enumerator.hasMoreElements()) {
-        let win = enumerator.getNext().QueryInterface(Ci.nsIDOMWindow);
+        let win = enumerator.getNext();
         loadIntoWindow(win);
     }
 
     // Load into any new windows
-    wm.addListener(windowListener);
+    Services.wm.addListener(windowListener);
 }
 
 function install (data, reason) { }
@@ -52,8 +50,7 @@ function install (data, reason) { }
 function shutdown (data, reason) {
     if (aReason == APP_SHUTDOWN) return;
 
-    let ios = Cc['@mozilla.org/network/io-service;1'].getService(Ci.nsIIOService)
-    let resource = ios.getProtocolHandler("resource").
+    let resource = Services.io.getProtocolHandler("resource").
         QueryInterface(Ci.nsIResProtocolHandler);
     resource.setSubstitution(data.id.substring(0, data.id.indexOf('@')), null);
 }
