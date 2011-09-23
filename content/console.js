ec67dc3678de99110c63204c70adcb19c3b8b647
Eitan Isaacson <eitan@monotonous.org>
2011-09-23 16:32:45 -0700

    made console module more modern. observe prefs.

--- content/console.js
+++ content/console.js
@@ -2,49 +2,62 @@ Components.utils.import("resource://gre/modules/ctypes.jsm")
 
 var EXPORTED_SYMBOLS = ["console"];
 
-var console = {
-    _consoleService: Components.classes["@mozilla.org/consoleservice;1"].
-        getService(Components.interfaces.nsIConsoleService),
-    _do_dump: Components.classes["@mozilla.org/preferences-service;1"].
-        getService(Components.interfaces.nsIPrefBranch).
-        getBoolPref("browser.dom.window.dump.enabled"),
-    _android_log: null,
+function Console() {
+    this._consoleService = Components.classes["@mozilla.org/consoleservice;1"].
+        getService(Components.interfaces.nsIConsoleService);
 
-    log: function (s) {
-        if (this._android_log)
-            this._android_log(3, "TalkToMe", String(s));
-        else if (this._do_dump)
-            dump (String(s) + '\n');
-        else
-            this._consoleService.logStringMessage(String(s));
-    },
+    this._prefs =  Components.classes["@mozilla.org/preferences-service;1"].
+        getService(Components.interfaces.nsIPrefBranch)
+        .QueryInterface(Components.interfaces.nsIPrefBranch2);
 
-    _toString: function (obj) {
-        if (obj === null) 
-            return "null";
-        else if (obj.call)
-            return "[ function ]";
-        else
-            return String(obj);
-    },
+    this._do_dump = this._prefs.getBoolPref("browser.dom.window.dump.enabled");
 
-    dumpObj: function (obj, dump_const) {
-        this.log(obj);
-        for (var prop in obj) {
-            if (prop.toUpperCase() == prop && !dump_const) // Probably a constant
-                continue;
-            this.log(" " + prop + ": " + this._toString(obj[prop]));
-        }
+    try {
+        let liblog=ctypes.open('liblog.so');
+        this._android_log = liblog.declare("__android_log_write",
+                                           ctypes.default_abi,
+                                           ctypes.int32_t,
+                                           ctypes.int32_t,
+                                           ctypes.char.ptr,
+                                           ctypes.char.ptr);
+    } catch (e) {
+    }
+
+    this._prefs.addObserver("", this, false);
+}
+
+Console.prototype.observe = function(aSubject, aTopic, aData) {  
+    if(aTopic != "nsPref:changed" || aData != "browser.dom.window.dump.enabled")
+        return;  
+
+    this._do_dump = this._prefs.getBoolPref("browser.dom.window.dump.enabled");
+}  
+
+Console.prototype.log = function (s) {
+    if (this._android_log)
+        this._android_log(3, "TalkToMe", String(s));
+    else if (this._do_dump)
+        dump (String(s) + '\n');
+    else
+        this._consoleService.logStringMessage(String(s));
+}
+
+Console.prototype._toString = function (obj) {
+    if (obj === null) 
+        return "null";
+    else if (obj.call)
+        return "[ function ]";
+    else
+        return String(obj);
+}
+
+Console.prototype.dumpObj = function (obj) {
+    this.log(obj);
+    for (var prop in obj) {
+        if (prop.toUpperCase() == prop && !dump_const) // Probably a constant
+            continue;
+        this.log(" " + prop + ": " + this._toString(obj[prop]));
     }
 }
 
-try {
-    liblog=ctypes.open('liblog.so');
-    console._android_log = liblog.declare("__android_log_write",
-                                          ctypes.default_abi,
-                                          ctypes.int32_t,
-                                          ctypes.int32_t,
-                                          ctypes.char.ptr,
-                                          ctypes.char.ptr);
-} catch (e) {
-}
+var console = new Console();
