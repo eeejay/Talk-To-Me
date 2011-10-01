def97d4c388ff6fdd331b7fd823cfe5562da29f3
Eitan Isaacson <eitan@monotonous.org>
2011-10-01 15:32:45 -0700

    factored out highlighter

---
+++ content/highlighter.js
@@ -0,0 +1,113 @@
+Components.utils.import("resource://talktome/content/console.js");
+
+EXPORTED_SYMBOLS = ["Highlighter"];
+
+function Highlighter(window) {
+    this.window = window;
+
+    // TODO: Make into prefs
+    this.padding = 4;
+    this.borderWidth = 2;
+    this.borderRadius = 4;
+    this.borderColor = "orange";
+    this.borderShadow = true;
+
+    let document = window.document;
+    let stack = window.document.getElementById('stack');
+
+    let highlightLayer = document.createElementNS(
+        "http://www.w3.org/1999/xhtml", "div");
+    highlightLayer.style.pointerEvents = "none";
+    highlightLayer.style.MozStackSizing = 'ignore';
+    highlightLayer.style.position = "relative";
+    stack.appendChild(highlightLayer);
+    
+    this._highlightRect = document.createElementNS(
+        "http://www.w3.org/1999/xhtml", "div");
+    this._highlightRect.style.position = "absolute";
+    this._highlightRect.style.borderStyle = "solid";
+    this._highlightRect.style.pointerEvents = "none";
+    this._highlightRect.style.display = "none";
+    highlightLayer.appendChild(this._highlightRect);
+
+    // style it
+    this._highlightRect.style.borderColor = this.borderColor;
+    this._highlightRect.style.borderRadius = this.borderRadius + "px";
+    this._highlightRect.style.borderWidth = this.borderWidth + "px";
+    
+    if (this.borderShadow) {
+        this._highlightRect.style.boxShadow = "1px 1px 1px #444";
+
+        // Create inset for inner shadow
+        let inset = document.createElementNS(
+            "http://www.w3.org/1999/xhtml", "div");
+        inset.style.width = "100%";
+        inset.style.height = "100%";
+        inset.style.borderRadius = (this.borderRadius/2) + "px";
+        inset.style.boxShadow = "inset 1px 1px 1px #444";
+        this._highlightRect.appendChild(inset);
+    }
+}
+
+Highlighter.prototype.highlight = function (bounds) {
+    let rect = this._transformContentRect(bounds.top - this.padding,
+                                          bounds.left - this.padding,
+                                          bounds.bottom + this.padding,
+                                          bounds.right + this.padding);
+
+    
+    let clipping = this._clip(rect);
+    
+    if (clipping.top != 0 || clipping.left != 0 ||
+        clipping.bottom != 0 || clipping.right != 0) {
+        let view = this.window.Browser.selectedTab.browser.getRootView();
+        view.scrollBy(Math.round(clipping.left || clipping.right),
+                      Math.round(clipping.top || clipping.bottom));
+
+        rect = this._transformContentRect(bounds.top - padding,
+                                          bounds.left - padding,
+                                          bounds.bottom + padding,
+                                          bounds.right + padding);
+    }
+
+    this.clear();
+    this._highlight(rect);
+};
+
+Highlighter.prototype._clip = function (rect) {
+    let bcr = this.window.Browser.selectedTab.browser.getBoundingClientRect();
+
+    return {
+        left: ((rect.left >= bcr.left) ? 0 : rect.left - bcr.left),
+        top: ((rect.top >= bcr.top) ? 0 : rect.top - bcr.top),
+        right: ((rect.right <= bcr.right) ? 0 : rect.right - bcr.right),
+        bottom: ((rect.bottom <= bcr.height) ? 0 : rect.bottom - bcr.height)
+    };
+};
+
+Highlighter.prototype._transformContentRect = function (top, left, bottom, right) {
+    let t1 = this.window.Browser.selectedTab.browser.transformBrowserToClient(
+        left, top);
+    let t2 = this.window.Browser.selectedTab.browser.transformBrowserToClient(
+        right, bottom);
+    
+    return { top: t1.y,
+             left: t1.x,
+             bottom: t2.y,
+             right: t2.x };
+};
+
+Highlighter.prototype._highlight = function (rect){    
+    this._highlightRect.style.display = "none";
+    this._highlightRect.style.top = (rect.top - this.borderWidth/2) + "px";
+    this._highlightRect.style.left = (rect.left - this.borderWidth/2) + "px";
+    this._highlightRect.style.width =
+        (rect.right - rect.left - this.borderWidth) + "px";
+    this._highlightRect.style.height =
+        (rect.bottom - rect.top - this.borderWidth) + "px";
+    this._highlightRect.style.display = "block";
+};
+
+Highlighter.prototype.clear = function () {
+    let border = 2;
+}
--- content/main.js
+++ content/main.js
@@ -1,12 +1,13 @@
-Components.utils.import("resource://talktome/content/console.js");
-
 try {
+    Components.utils.import("resource://talktome/content/console.js");
     Components.utils.import("resource://talktome/content/input_mangler.js");
+    Components.utils.import("resource://talktome/content/highlighter.js");
 } catch (e) {
-    console.log("aah " + e);
+    console.printException(e);
 }
+
 var TalkToMe = {
-    _highlightRect: null,
+    _highlighter: null,
 
     onLoad : function(aEvent) {
         this.input_mangler = new InputMangler(window);
@@ -18,40 +19,7 @@ var TalkToMe = {
     },
 
     onUIReady : function(aEvent) {
-        try {
-            let document = window.document;
-            let stack = window.document.getElementById('stack');
-
-
-            let highlightLayer = document.createElementNS(
-                "http://www.w3.org/1999/xhtml", "div");
-            highlightLayer.style.pointerEvents = "none";
-            highlightLayer.style.MozStackSizing = 'ignore';
-            highlightLayer.style.position = "relative";
-            stack.appendChild(highlightLayer);
-
-            this._highlightRect = document.createElementNS(
-                "http://www.w3.org/1999/xhtml", "div");
-            this._highlightRect.style.position = "absolute";
-            this._highlightRect.style.borderStyle = "solid";
-            this._highlightRect.style.borderColor = "orange";
-            this._highlightRect.style.borderRadius = "4px";
-            this._highlightRect.style.boxShadow = "1px 1px 1px #444";
-            this._highlightRect.style.pointerEvents = "none";
-            this._highlightRect.style.display = "none";
-            highlightLayer.appendChild(this._highlightRect);
-
-            let inset = document.createElementNS(
-                "http://www.w3.org/1999/xhtml", "div");
-            inset.style.width = "100%";
-            inset.style.height = "100%";
-            inset.style.borderRadius = "2px";
-            inset.style.boxShadow = "inset 1px 1px 1px #444";
-            this._highlightRect.appendChild(inset);
-
-        } catch (e) {
-            console.log("Error adding highlighter: " + e);
-        }
+        this._highlighter = new Highlighter (window);
     },
 
     onUIReadyDelayed : function(aEvent) {
@@ -63,71 +31,12 @@ var TalkToMe = {
             let bounds = aMessage.json.bounds;
 
             tts.speak(phrase);
-
-            const padding = 4;
-
-            // translate coords
-            let rect = this._transformContentRect(bounds.top - padding,
-                                                  bounds.left - padding,
-                                                  bounds.bottom + padding,
-                                                  bounds.right + padding);
-
-
-            let clipping = this._clip(rect);
-
-            if (clipping.top != 0 || clipping.left != 0 ||
-                clipping.bottom != 0 || clipping.right != 0) {
-                let view = window.Browser.selectedTab.browser.getRootView();
-                view.scrollBy(Math.round(clipping.left || clipping.right),
-                              Math.round(clipping.top || clipping.bottom));
-
-                rect = this._transformContentRect(bounds.top - padding,
-                                                  bounds.left - padding,
-                                                  bounds.bottom + padding,
-                                                  bounds.right + padding);
-            }
-
-            this._highlight(rect);
+            this._highlighter.highlight(bounds);
         } catch (e) {
-            console.log("Error presenting: " + e);
+            console.log ("Error::receiveMessage: " + e);
         }
-    },
-
-    _clip: function (rect) {
-        let bcr = window.Browser.selectedTab.browser.getBoundingClientRect();
-
-        return {
-            left: ((rect.left >= bcr.left) ? 0 : rect.left - bcr.left),
-            top: ((rect.top >= bcr.top) ? 0 : rect.top - bcr.top),
-            right: ((rect.right <= bcr.right) ? 0 : rect.right - bcr.right),
-            bottom: ((rect.bottom <= bcr.height) ? 0 : rect.bottom - bcr.height)
-        };
-    },
-
-    _transformContentRect: function (top, left, bottom, right) {
-        let t1 = window.Browser.selectedTab.browser.transformBrowserToClient(
-            left, top);
-        let t2 = window.Browser.selectedTab.browser.transformBrowserToClient(
-            right, bottom);
-
-        return { top: t1.y,
-                 left: t1.x,
-                 bottom: t2.y,
-                 right: t2.x };
-    },
-
-    _highlight: function (rect){
-        let border = 2;
-
-        this._highlightRect.style.display = "none";
-        this._highlightRect.style.borderWidth = border + "px";
-        this._highlightRect.style.top = (rect.top - border/2) + "px";
-        this._highlightRect.style.left = (rect.left - border/2) + "px";
-        this._highlightRect.style.width = (rect.right - rect.left - border) + "px";
-        this._highlightRect.style.height = (rect.bottom - rect.top - border) + "px";
-        this._highlightRect.style.display = "block";
     }
-};
+}
 
 // Setup the main event listeners
 window.addEventListener("load", function(e) {
@@ -140,7 +49,11 @@ window.addEventListener("load", function(e) {
 }, false);
 
 window.addEventListener("UIReady", function(e) {
-  TalkToMe.onUIReady(e);
+    try {
+        TalkToMe.onUIReady(e);
+    } catch (e) {
+        console.log ("Error::onUIReady: " + e);
+    }
 }, false);
 
 window.addEventListener("UIReadyDelayed", function(e) {
