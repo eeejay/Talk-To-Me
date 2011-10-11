826ca679445df7bf7cedd0122f1adab0ee870c65
Eitan Isaacson <eitan@monotonous.org>
2011-10-11 10:55:59 -0700

    show chrome when doing three finger swipe or alt+left/right

--- content/input_manager.js
+++ content/input_manager.js
@@ -3,15 +3,41 @@ Components.utils.import("resource://talktome/content/closure_utils.js");
 
 EXPORTED_SYMBOLS=["InputManager"];
 
+function _SidebarToggler (window) {
+    let document = window.document;
+    this.window = window;
+
+    this._elements = [document.getElementById('controls-sidebar'),
+                      document.getElementById('page-stack'),
+                      document.getElementById('tabs-sidebar')];
+    this._current = 1;
+}
+
+_SidebarToggler.prototype = {
+    left: function left () {
+        this._current = Math.max(0, --this._current);
+        this.window.Browser.controlsScrollboxScroller
+            .ensureElementIsVisible(this._elements[this._current]);
+    },
+    right: function right () {
+        this._current = Math.min(this._elements.length - 1, ++this._current);
+        this.window.Browser.controlsScrollboxScroller
+            .ensureElementIsVisible(this._elements[this._current]);
+    }
+}
+
 function InputManager (window) {
+    this.window = window;
+    this._sidebarToggler = new _SidebarToggler(window);
+
     window.addEventListener('keypress',
-                            Callback(this.keypressHandler), false);
+                            Callback(this.keypressHandler, this), false);
     window.addEventListener('TalkToMe::Swipe',
-                            Callback(this.swipeHandler),false);
+                            Callback(this.swipeHandler, this),false);
     window.addEventListener('TalkToMe::Tap',
-                            Callback(this.tapHandler), false);
+                            Callback(this.tapHandler, this), false);
     window.addEventListener('TalkToMe::Dwell',
-                            Callback(this.dwellHandler), false);
+                            Callback(this.dwellHandler, this), false);
 }
 
 InputManager.prototype.dwellHandler = function (e) {
@@ -36,13 +62,24 @@ InputManager.prototype.swipeHandler = function (e) {
         switch (detail.direction) {
         case "right":
             console.log ("next");
-            this.tts.playTick();
+            this.window.tts.playTick();
             mm.sendAsyncMessage("TalkToMe:Navigate", { direction : "next" });
             break;
         case "left":
             console.log ("prev");
-            this.tts.playTick();
+            this.window.tts.playTick();
             mm.sendAsyncMessage("TalkToMe:Navigate", { direction : "prev" });
+            break;
+        default:
+            break;
+        }
+    } else if (detail.fingers == 3) {
+        switch (detail.direction) {
+        case "right":
+            this._sidebarToggler.right();
+            break;
+        case "left":
+            this._sidebarToggler.left();
             break;
         default:
             break;
@@ -61,6 +98,12 @@ InputManager.prototype.keypressHandler = function (e) {
         case e.DOM_VK_UP:
             mm.sendAsyncMessage("TalkToMe:Navigate", { direction : "prev" });
             console.log ("prev");
+            break;
+        case e.DOM_VK_LEFT:
+            this._sidebarToggler.left();
+            break;
+        case e.DOM_VK_RIGHT:
+            this._sidebarToggler.right();
             break;
         case 13: // For some reason DOM_VK_ENTER maps to 14 ??
             mm.sendAsyncMessage("TalkToMe:Activate");
